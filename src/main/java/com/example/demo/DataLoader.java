b8fc14d1554c01d7c9ca46f4bcd6b96bc48d59bd
prasantib <prasanti.babu@gmail.com>
2019-10-09 16:16:11 -0400

    StringBoot_304

---
+++ src/main/java/com/example/demo/DataLoader.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+    @Autowired
+    CourseRepository repository;
+
+    @Override
+    public void run(String... strings) throws Exception {
+        Course course;
+        course = new Course("Astrophysics", "Neil D Tyson", "Just a course on stars", 3);
+        repository.save(course);
+
+        course = new Course("Freshman English", "Geraldine Pegram", "Learn your Language children", 3);
+        repository.save(course);
+    }
+}
---
+++ src/main/java/com/example/demo/StringBoot303Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringBoot303Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringBoot303Application.class, args);
+    }
+
+}
